899df70faf311c94b26679a745cc4734d6c2d42c
Charles Clarke <charles4clarke@gmail.com>
2025-03-02 00:57:13 -0700

    cleaned up DAO files

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -2,7 +2,6 @@ package dataaccess;
 
 import model.AuthData;
 
-import javax.xml.crypto.Data;
 
 public interface AuthDAO {
 
--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -2,4 +2,14 @@ package model;
 
 import chess.ChessGame;
 
-public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {}
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+
+    public GameData setWhiteUsername(String whiteUsername) {
+        return new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+    }
+
+    public GameData setBlackUsername(String blackUsername) {
+        return new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+    }
+
+}
