3573e126853d8ee946324ece6a19370edc350f65
Charles Clarke <charles4clarke@gmail.com>
2025-03-02 16:30:07 -0700

    Implemented Server.java with endpoints to each handler

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,12 @@
 package server;
 
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import server.handlers.*;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 import spark.*;
 
 public class Server {
@@ -9,7 +16,22 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
+
+        UserService userService = new UserService(userDAO, authDAO);
+        GameService gameService = new GameService(gameDAO);
+        ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
+
         // Register your endpoints and handle exceptions here.
+        new RegisterHandler(userService);
+        new LoginHandler(userService);
+        new LogoutHandler(userService);
+        new CreateGameHandler(gameService);
+        new ListGamesHandler(gameService);
+        new JoinGameHandler(gameService);
+        new ClearHandler(clearService);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
