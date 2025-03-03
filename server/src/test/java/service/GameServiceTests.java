2c6059063a960350992d75f93cb770f024250dbb
Charles Clarke <charles4clarke@gmail.com>
2025-03-03 12:50:20 -0700

    fixed GameServiceTests

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,50 +1,24 @@
 package service;
 
-import chess.ChessGame;
 import dataaccess.DataAccessException;
-import dataaccess.GameDAO;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
-import model.AuthData;
-import model.GameData;
 import org.junit.jupiter.api.*;
-import passoff.model.*;
-import passoff.server.TestServerFacade;
-import server.Server;
 
-import java.net.HttpURLConnection;
-import java.util.*;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GameServiceTests {
     private static MemoryAuthDAO authDAO;
     private static MemoryGameDAO gameDAO;
-    private static Server server;
     private static GameService gameService;
-    private static TestServerFacade serverFacade;
-
-    @BeforeAll
-    public static void init() {
-        server = new Server();
-        var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
-
-
-        serverFacade = new TestServerFacade("localhost", Integer.toString(port));
-        gameDAO = new MemoryGameDAO();
-        authDAO = new MemoryAuthDAO();
-        gameService = new GameService(gameDAO, authDAO);
-    }
-
-    @AfterAll
-    static void stopServer() {
-        server.stop();
-    }
 
     @BeforeEach
     public void setup() {
-        serverFacade.clear();
+        gameDAO = new MemoryGameDAO();
+        authDAO = new MemoryAuthDAO();
+        gameService = new GameService(gameDAO, authDAO);
+
         try {
             gameDAO.clear();
             authDAO.clear();
@@ -185,37 +159,5 @@ public class GameServiceTests {
 
 
 
-    private void assertHttpOk(TestResult result) {
-        Assertions.assertEquals(HttpURLConnection.HTTP_OK, serverFacade.getStatusCode(),
-                "Server response code was not 200 OK (message: %s)".formatted(result.getMessage()));
-        Assertions.assertFalse(result.getMessage() != null &&
-                        result.getMessage().toLowerCase(Locale.ROOT).contains("error"),
-                "Result returned an error message");
-    }
-
-    private void assertHttpBadRequest(TestResult result) {
-        assertHttpError(result, HttpURLConnection.HTTP_BAD_REQUEST, "Bad Request");
-    }
-
-    private void assertHttpUnauthorized(TestResult result) {
-        assertHttpError(result, HttpURLConnection.HTTP_UNAUTHORIZED, "Unauthorized");
-    }
-
-    private void assertHttpForbidden(TestResult result) {
-        assertHttpError(result, HttpURLConnection.HTTP_FORBIDDEN, "Forbidden");
-    }
-
-    private void assertHttpError(TestResult result, int statusCode, String message) {
-        Assertions.assertEquals(statusCode, serverFacade.getStatusCode(),
-                "Server response code was not %d %s (message: %s)".formatted(statusCode, message, result.getMessage()));
-        Assertions.assertNotNull(result.getMessage(), "Invalid Request didn't return an error message");
-        Assertions.assertTrue(result.getMessage().toLowerCase(Locale.ROOT).contains("error"),
-                "Error message didn't contain the word \"Error\"");
-    }
-
-    private void assertAuthFieldsMissing(TestAuthResult result) {
-        Assertions.assertNull(result.getUsername(), "Response incorrectly returned username");
-        Assertions.assertNull(result.getAuthToken(), "Response incorrectly return authentication String");
-    }
 
 }
