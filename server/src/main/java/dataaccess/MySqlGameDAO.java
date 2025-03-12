83bed7d724645ed7c2a1505dfb20756db5c02f8a
Charles Clarke <charles4clarke@gmail.com>
2025-03-12 16:40:05 -0600

    MySqlGameDAO executeUpdate

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -3,8 +3,13 @@ package dataaccess;
 import com.google.gson.Gson;
 import model.GameData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.sql.Types.NULL;
 
 public class MySqlGameDAO implements GameDAO {
 
@@ -20,7 +25,7 @@ public class MySqlGameDAO implements GameDAO {
 
     @Override
     public List<GameData> listGames() throws DataAccessException {
-        return List.of();
+
     }
 
     @Override
@@ -46,6 +51,28 @@ public class MySqlGameDAO implements GameDAO {
     @Override
     public void updateGame(GameData game) {
 
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            for (int i = 0; i < params.length; i++) {
+                var param = params[i];
+                if (param instanceof String p) ps.setString(i + 1, p);
+                else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                else if (param == null) ps.setNull(i + 1, NULL);
+            }
+            ps.executeUpdate();
+
+            try (var rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+            }
+            return 0;
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 
 
