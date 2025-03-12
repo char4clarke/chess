4e259957652b7c02c99c9ad2e5e5e23c8552577c
Charles Clarke <charles4clarke@gmail.com>
2025-03-12 16:27:42 -0600

    MySqlGameDAO configureDatabase and such

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -1,10 +1,23 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.GameData;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class MySqlGameDAO implements GameDAO {
+
+    private final Gson gson = new Gson();
+
+    public MySqlGameDAO() {
+        try {
+            configureDatabase();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public List<GameData> listGames() throws DataAccessException {
         return List.of();
@@ -33,5 +46,33 @@ public class MySqlGameDAO implements GameDAO {
     @Override
     public void updateGame(GameData game) {
 
+    }
+
+
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+                gameID INT AUTO_INCREMENT PRIMARY KEY,
+                whiteUsername VARCHAR(255),
+                blackUsername VARCHAR(255),
+                gameName VARCHAR(255) NOT NULL,
+                stateJSON TEXT
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLAtE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
