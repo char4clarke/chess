98f74b79e105f902b78b6509f9f964c02547fd8a
Charles Clarke <charles4clarke@gmail.com>
2025-03-13 11:25:48 -0600

    MySqlUserDAO updated some methods

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -2,6 +2,10 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.SQLException;
+
+import static java.sql.Types.NULL;
+
 public class MySqlUserDAO implements UserDAO {
     @Override
     public void createUser(UserData user) throws DataAccessException {
@@ -16,5 +20,50 @@ public class MySqlUserDAO implements UserDAO {
     @Override
     public void clear() throws DataAccessException {
 
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                return ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+                username VARCHAR(256) NOT NULL,
+                password VARCHAR(256) NOT NULL,
+                email VARCHAR (256),
+                PRIMARY KEY (username),
+                INDEX(email)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLAtE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
