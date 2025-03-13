4a3c28d8ab372c68c991858e0defc788fcfac697
Charles Clarke <charles4clarke@gmail.com>
2025-03-13 15:05:15 -0600

    DBUserTests get user tests

--- server/src/test/java/dataaccess/DBUserTests.java
+++ server/src/test/java/dataaccess/DBUserTests.java
@@ -34,4 +34,23 @@ public class DBUserTests {
             Assertions.assertTrue(e.getMessage().contains("Error:"));
         }
     }
+
+    @Test
+    @Order(3)
+    @DisplayName("Get User (Positive)")
+    public void getUserPositive() throws DataAccessException {
+        UserData user = new UserData("username", "password", "type");
+        userDAO.createUser(user);
+        UserData getUser = userDAO.getUser("username");
+        Assertions.assertNotNull(getUser);
+        Assertions.assertEquals("username", getUser.username());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Get User (Negative)")
+    public void getUserNegative() throws DataAccessException {
+        UserData getUser = userDAO.getUser("username");
+        Assertions.assertNull(getUser);
+    }
 }
