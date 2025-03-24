5dc296177fcc95001992929912473185cf15f241
Charles Clarke <charles4clarke@gmail.com>
2025-03-24 13:59:39 -0600

    PostClient

--- client/src/main/java/ui/PostClient.java
+++ client/src/main/java/ui/PostClient.java
@@ -1,4 +1,15 @@
 package ui;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PostClient {
+    private final ServerFacade serverFacade;
+    private final String authToken;
+    private final Map<Integer, Integer> gameIDMap = new HashMap<>();
+
+    public PostClient(ServerFacade serverFacade, String authToken) {
+        this.serverFacade = serverFacade;
+        this.authToken = authToken;
+    }
 }
