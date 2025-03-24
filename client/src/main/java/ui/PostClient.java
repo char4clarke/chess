b71de535f8fc50c17eaafeb78d7027383a4deddd
Charles Clarke <charles4clarke@gmail.com>
2025-03-24 14:07:05 -0600

    PostClient loop

--- client/src/main/java/ui/PostClient.java
+++ client/src/main/java/ui/PostClient.java
@@ -1,7 +1,11 @@
 package ui;
 
+import exception.ResponseException;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import java.util.*;
 
 public class PostClient {
     private final ServerFacade serverFacade;
@@ -11,5 +15,24 @@ public class PostClient {
     public PostClient(ServerFacade serverFacade, String authToken) {
         this.serverFacade = serverFacade;
         this.authToken = authToken;
+    }
+
+    public void run() {
+        System.out.println(" Welcome back! Type 'help' for available commands.");
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+
+        while (!command.equalsIgnoreCase("logout")) {
+            System.out.print("[LOGGED_IN} >>> ");
+            command = scanner.nextLine();
+
+            try {
+                executeCommand(command);
+            } catch (ResponseException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        System.out.println("Logging out...");
+        handleLogout();
     }
 }
