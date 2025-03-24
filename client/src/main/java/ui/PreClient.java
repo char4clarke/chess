d97793d7c8179d670581b29f5c9e47260dd5ba9f
Charles Clarke <charles4clarke@gmail.com>
2025-03-24 13:37:11 -0600

    started working on PreClient loop

--- client/src/main/java/ui/PreClient.java
+++ client/src/main/java/ui/PreClient.java
@@ -1,4 +1,38 @@
 package ui;
 
+import exception.ResponseException;
+import service.UserService.*;
+
+import java.util.Scanner;
+
 public class PreClient {
+    private final ServerFacade serverFacade;
+
+    public PreClient(ServerFacade serverFacade) {
+        this.serverFacade = serverFacade;
+    }
+
+    public void run() {
+        System.out.println(" Welcome to 240 chess. Type Help to get started. ");
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+
+        while (!command.equalsIgnoreCase("quit")) {
+            System.out.print("[LOGGED_OUT] >>> ");
+            command = scanner.nextLine();
+
+            try {
+                executeCommand(command);
+            } catch (ResponseException e) {
+                System.out.println("Error: " + e);
+            }
+        }
+
+        System.out.println("Goodbye!");
+    }
+
+
+
+
+
 }
