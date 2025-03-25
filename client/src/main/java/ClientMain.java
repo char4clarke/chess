2da5a0a79ffbd8e65cc39b0731f0ca42e0e75a55
Charles Clarke <charles4clarke@gmail.com>
2025-03-24 23:11:26 -0600

    updated client main

---
+++ client/src/main/java/ClientMain.java
@@ -0,0 +1,19 @@
+import chess.*;
+import dataaccess.AuthDAO;
+import dataaccess.MySqlAuthDAO;
+import dataaccess.MySqlUserDAO;
+import dataaccess.UserDAO;
+import ui.PreClient;
+import ui.ServerFacade;
+import service.UserService;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        var serverUrl = "http://localhost:8080";
+        UserDAO userDAO = new MySqlUserDAO();
+        AuthDAO authDAO = new MySqlAuthDAO();
+        UserService userService = new UserService(userDAO, authDAO);
+        ServerFacade serverFacade = new ServerFacade(serverUrl);
+        new PreClient(serverFacade, userService).run();
+    }
+}
