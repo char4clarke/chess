f7840a1e511cf3d3d21a5baed9057f44f5259277
Charles Clarke <charles4clarke@gmail.com>
2025-03-25 10:59:33 -0600

    register tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,8 +1,10 @@
 package client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
 import service.UserService.*;
 import server.Server;
+import ui.ServerFacade;
 
 
 public class ServerFacadeTests {
@@ -16,12 +18,12 @@ public class ServerFacadeTests {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        facade = new ServerFacade("http://localhost:" + port);
+        serverFacade = new ServerFacade("http://localhost:" + port);
     }
 
     @BeforeEach
-    void clearDataBase() {
-        facade.clear();
+    void clearDataBase() throws ResponseException {
+        serverFacade.clear();
     }
 
     @AfterAll
@@ -29,6 +31,21 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+
+    @Test
+    public void registerPositive() throws Exception {
+        RegisterRequest request = new RegisterRequest("player1", "password", "p1@email.com");
+        RegisterResult result = serverFacade.register(request);
+        Assertions.assertNotNull(result.authToken());
+        Assertions.assertTrue(result.authToken().length() > 10);
+    }
+
+    @Test
+    public void registerNegative() throws Exception {
+        RegisterRequest request = new RegisterRequest("player1", null, "p1@email.com");
+        ResponseException e = Assertions.assertThrows(ResponseException.class, () -> serverFacade.register(request));
+        Assertions.assertTrue(e.getMessage().contains("Error"));
+    }
 
 
 
