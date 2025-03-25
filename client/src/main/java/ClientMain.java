9dc88eed3d8b0b1d132c31844ead2b043531cc41
Charles Clarke <charles4clarke@gmail.com>
2025-03-25 13:25:54 -0600

    fixed compatability stuff with the data models

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -1,17 +1,9 @@
-import dataaccess.AuthDAO;
-import dataaccess.MySqlAuthDAO;
-import dataaccess.MySqlUserDAO;
-import dataaccess.UserDAO;
 import ui.PreClient;
 import ui.ServerFacade;
-import service.UserService;
 
 public class ClientMain {
     public static void main(String[] args) {
         var serverUrl = "http://localhost:8080";
-        UserDAO userDAO = new MySqlUserDAO();
-        AuthDAO authDAO = new MySqlAuthDAO();
-        UserService userService = new UserService(userDAO, authDAO);
         ServerFacade serverFacade = new ServerFacade(serverUrl);
         new PreClient(serverFacade).run();
     }
