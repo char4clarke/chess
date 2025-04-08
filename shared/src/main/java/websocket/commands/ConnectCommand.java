6ada543dacff96c260163b1b8108ac7a731df777
Charles Clarke <charles4clarke@gmail.com>
2025-04-07 21:54:34 -0600

    updated ConnectCommand

--- shared/src/main/java/websocket/commands/ConnectCommand.java
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -1,4 +1,8 @@
 package websocket.commands;
 
-public class ConnectCommand {
+public class ConnectCommand extends UserGameCommand {
+
+    public ConnectCommand(String authToken, Integer gameID) {
+        super(CommandType.CONNECT, authToken, gameID);
+    }
 }
