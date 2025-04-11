f6b6fa45571aade504cd70cc7284e2324cad150b
Charles Clarke <charles4clarke@gmail.com>
2025-04-10 23:06:30 -0600

    updated ChessGameDeserializer

--- shared/src/main/java/websocket/deserializers/ChessGameDeserializer.java
+++ shared/src/main/java/websocket/deserializers/ChessGameDeserializer.java
@@ -10,17 +10,18 @@ public class ChessGameDeserializer implements JsonDeserializer<ChessGame> {
     @Override
     public ChessGame deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
             throws JsonParseException {
+
         JsonObject obj = json.getAsJsonObject();
+        ChessBoard board = context.deserialize(obj.get("board"), ChessBoard.class);
+        ChessGame.TeamColor teamTurn = ChessGame.TeamColor.valueOf(obj.get("teamTurn").getAsString());
+        boolean isGameOver = obj.get("isGameOver").getAsBoolean();
+
         ChessGame game = new ChessGame();
+        game.setBoard(board);
+        game.setTeamTurn(teamTurn);
+        game.setGameOver(isGameOver);
 
-        if (obj.has("board")) {
-            game.setBoard(context.deserialize(obj.get("board"), ChessBoard.class));
-        }
-
-        if (obj.has("teamTurn")) {
-            game.setTeamTurn(ChessGame.TeamColor.valueOf(obj.get("teamTurn").getAsString()));
-        }
-
+        System.out.println("[DEBUG] Deserialized ChessGame: " + game);
         return game;
     }
 }
