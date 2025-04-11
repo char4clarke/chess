86760ff4e9d5f47393cc7633f16466fe00b4df39
Charles Clarke <charles4clarke@gmail.com>
2025-04-10 23:07:09 -0600

    updated ConnectCommand

--- shared/src/main/java/websocket/commands/ConnectCommand.java
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -1,16 +1,12 @@
 package websocket.commands;
 
-import chess.ChessGame;
 
 public class ConnectCommand extends UserGameCommand {
-    ChessGame.TeamColor playerColor;
 
-    public ConnectCommand(String authToken, Integer gameID, String playerColor) {
+
+    public ConnectCommand(String authToken, Integer gameID) {
         super(CommandType.CONNECT, authToken, gameID);
-        this.playerColor = ChessGame.TeamColor.valueOf(playerColor);
     }
 
-    public ChessGame.TeamColor getPlayerColor() {
-        return playerColor;
-    }
+
 }
